d211b8807094894ddee5e362d204729ff76e3bb0
bora <nhatbaoxxd@gmail.com>
2024-02-18 17:13:34 +0700

    11. Refine

--- src/11-refine.problem.ts
+++ src/11-refine.problem.ts
@@ -6,7 +6,12 @@ import { z } from "zod";
 const Form = z.object({
   password: z.string(),
   confirmPassword: z.string(),
-});
+}).refine((data) => {
+  return data.password === data.confirmPassword;
+}, {
+  message: "Passwords don't match",
+  path: ["confirmPassword"],
+})
 //^ 🕵️‍♂️
 
 export const validateFormInput = (values: unknown) => {
